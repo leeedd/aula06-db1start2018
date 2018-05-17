d1b85ed7a07d799759f35033a076bb5ab04fa91e
Aluno <Aluno>
2018-05-17 15:21:25 -0300

    Inserindo mascara de cep

---
+++ src/br/com/db1/Start/recibo/Emitente.java
@@ -0,0 +1,14 @@
+package br.com.db1.Start.recibo;
+
+public class Emitente {
+
+	private String nome;
+	
+	private String documento;
+	
+	private byte[] assinatura;
+	
+	public String getDocumentoFormatado(){
+		return "";
+	}
+}
